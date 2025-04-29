ddd27782e8cf5e9f070f14e02685181d0cc5d90f
FourAndHalf <jinsoneb@gmail.com>
2025-04-29 13:46:27 +0530

    Added the component pages needed and basic routing of the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'pricing', component: PricingComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '**', redirectTo: '' }
+];
