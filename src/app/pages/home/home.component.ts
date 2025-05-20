854321da74b358b26a02af86150e3ad75cd920fc
FourAndHalf <jinsoneb@gmail.com>
2025-05-20 23:09:50 +0530

    Added the service functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { shortUrlService } from '../../api/shorturl.service';
 import { createShortUrl } from '../../api/models/createShortUrl.model';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 export class HomeComponent {
-  createShortUrlDto: createShortUrl = {
-    originalUrl: '',
-    daysToExpiry: 7,
-  };
+  txtOriginalUrl: string = '';
+
+  constructor(private pShortUrlService: shortUrlService) {}
+
+  submitOriginalUrl() {
+    const createShortUrlDto: createShortUrl = {
+      originalUrl: this.txtOriginalUrl,
+      daysToExpiry: 7,
+      shortenUrl: '',
+      user: '',
+      userType: ''
+    };
+
+    this.pShortUrlService.createShortCode(createShortUrlDto).subscribe(
+      (res) => console.log("Post Success:", res),
+      (err) => console.error("Post Error:", err)
+    )
+  }
 }
 
-
-createShortUrl() {
-
-}
